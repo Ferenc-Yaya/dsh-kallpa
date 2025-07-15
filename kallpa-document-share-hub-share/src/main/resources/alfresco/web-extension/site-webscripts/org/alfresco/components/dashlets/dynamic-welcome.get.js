bff9527a72dfa990146d77ad2555d3f7be0e219b
Ferenc <ferenc_yaya@hotmail.com>
2025-07-15 11:19:42 -0500

    elimine el mensaje de introduccion de alfresco

---
+++ kallpa-document-share-hub-share/src/main/resources/alfresco/web-extension/site-webscripts/org/alfresco/components/dashlets/dynamic-welcome.get.js
@@ -0,0 +1,14 @@
+function main()
+{
+   model.showDashlet = false;
+   model.userIsMember = false;
+   model.userIsSiteManager = false;
+   model.userIsSiteConsumer = false;
+   model.columns = [];
+   model.dashboardUrl = "";
+   model.dashboardId = "";
+   model.dashboardType = args.dashboardType;
+   model.widgets = [];
+}
+
+main();
